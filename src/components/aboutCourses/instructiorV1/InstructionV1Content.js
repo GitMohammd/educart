7b3afd4f33fff0d3dfebd5d8f8361d86d7a51505
shamim <shamim@createdbycocoon.com>
2022-12-19 13:17:20 +0600

    created instructor.json file

--- src/components/aboutCourses/instructiorV1/InstructionV1Content.js
+++ src/components/aboutCourses/instructiorV1/InstructionV1Content.js
@@ -1,6 +1,58 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 
 function InstructionV1Content() {
+  const catRef = useRef();
+  const ratRef = useRef();
+  const [isCatOpen, setIscatOpen] = useState(false);
+  const [isRatOpen, setIsRetOpen] = useState(false);
+  const [courses, setCourses] = useState([]);
+
+  useEffect(() => {
+    fetch("/allInstructor.json")
+      .then((res) => res.json())
+      .then((data) => setCourses(data));
+  }, []);
+  useEffect(() => {
+    const closeCat = (e) => {
+      if (!catRef.current.contains(e.target)) {
+        setIscatOpen(false);
+      }
+    };
+    document.body.addEventListener("click", closeCat);
+    return () => {
+      document.body.removeEventListener("click", closeCat);
+    };
+  }, []);
+  useEffect(() => {
+    const closerat = (e) => {
+      if (!ratRef.current.contains(e.target)) {
+        setIsRetOpen(false);
+      }
+    };
+    document.body.addEventListener("click", closerat);
+    return () => {
+      document.body.removeEventListener("click", closerat);
+    };
+  }, []);
+  const [cataValue, setCataValue] = useState("");
+  const [ratvalue, setRatValue] = useState("");
+  const newCourse = courses
+    ?.filter((catacourse) => {
+      return cataValue === ""
+        ? catacourse
+        : catacourse.popular?.includes(cataValue);
+    })
+    .filter((ratcourse) => {
+      return ratvalue === ""
+        ? ratcourse
+        : ratcourse.populerCourseRating?.includes(ratvalue);
+    })
+    .map((course) => {
+      return course;
+    });
+
   return (
     <section className="layout-pt-md layout-pb-lg">
       <div data-anim-wrap className="container">
@@ -26,41 +78,64 @@ function InstructionV1Content() {
                   </button>
                 </form>
               </div>
-              <div className="col-auto">
-                <div className="dropdown js-dropdown js-category-active">
+              <div ref={catRef} className="col-auto">
+                <div
+                  onClick={() => setIscatOpen((prev) => !prev)}
+                  className="dropdown js-dropdown js-category-active"
+                >
                   <div
                     className="dropdown__button d-flex items-center text-14 rounded-8 px-20 py-10 text-14 lh-12"
                     data-el-toggle=".js-category-toggle"
                     data-el-toggle-active=".js-category-active"
                   >
-                    <span className="js-dropdown-title">Category</span>
-                    <i className="icon text-9 ml-40 icon-chevron-down"></i>
+                    <span className="js-dropdown-title">
+                      {cataValue === ""
+                        ? "Category"
+                        : cataValue.charAt(0).toUpperCase() +
+                          cataValue.slice(1)}
+                    </span>
+                    {isCatOpen ? (
+                      <KeyboardArrowUpIcon />
+                    ) : (
+                      <KeyboardArrowDownIcon />
+                    )}
+                    {/* <i className="icon text-9 ml-40 icon-chevron-down"></i> */}
                   </div>
 
-                  <div className="toggle-element -dropdown -dark-bg-dark-2 -dark-border-white-10 js-click-dropdown js-category-toggle">
-                    <div className="text-14 y-gap-15 js-dropdown-list">
-                      <div>
-                        <a href="#" className="d-block js-dropdown-link">
+                  <div
+                    className={
+                      isCatOpen
+                        ? "toggle-element -dropdown -dark-bg-dark-2 -dark-border-white-10 js-click-dropdown js-category-toggle -is-el-visible"
+                        : "toggle-element -dropdown -dark-bg-dark-2 -dark-border-white-10 js-click-dropdown js-category-toggle "
+                    }
+                  >
+                    <div class="text-14 y-gap-15 js-dropdown-list">
+                      <div onClick={() => setCataValue("animation")}>
+                        <button class="d-block js-dropdown-link">
                           Animation
-                        </a>
+                        </button>
                       </div>
 
-                      <div>
-                        <a href="#" className="d-block js-dropdown-link">
-                          Design
-                        </a>
+                      <div onClick={() => setCataValue("design")}>
+                        <button class="d-block js-dropdown-link">Design</button>
                       </div>
 
-                      <div>
-                        <a href="#" className="d-block js-dropdown-link">
+                      <div onClick={() => setCataValue("illustration")}>
+                        <button class="d-block js-dropdown-link">
                           Illustration
-                        </a>
+                        </button>
                       </div>
 
-                      <div>
-                        <a href="#" className="d-block js-dropdown-link">
+                      <div onClick={() => setCataValue("lifestyle")}>
+                        <button class="d-block js-dropdown-link">
+                          Lifestyle
+                        </button>
+                      </div>
+
+                      <div onClick={() => setCataValue("business")}>
+                        <button class="d-block js-dropdown-link">
                           Business
-                        </a>
+                        </button>
                       </div>
                     </div>
                   </div>
@@ -69,38 +144,48 @@ function InstructionV1Content() {
               <div className="col-auto">
                 <div className="dropdown js-dropdown js-category-active">
                   <div
+                    ref={ratRef}
+                    onClick={() => setIsRetOpen((prev) => !prev)}
                     className="dropdown__button d-flex items-center text-14 rounded-8 px-20 py-10 text-14 lh-12"
                     data-el-toggle=".js-category-toggle"
                     data-el-toggle-active=".js-category-active"
                   >
-                    <span className="js-dropdown-title">Sort by: Default</span>
-                    <i className="icon text-9 ml-40 icon-chevron-down"></i>
+                    <span className="js-dropdown-title">
+                      {" "}
+                      {ratvalue === ""
+                        ? "Rating"
+                        : ratvalue.charAt(0).toUpperCase() + ratvalue.slice(1)}
+                    </span>
+                    {isRatOpen ? (
+                      <KeyboardArrowUpIcon />
+                    ) : (
+                      <KeyboardArrowDownIcon />
+                    )}
+                    {/* <i className="icon text-9 ml-40 icon-chevron-down"></i> */}
                   </div>
 
-                  <div className="toggle-element -dropdown -dark-bg-dark-2 -dark-border-white-10 js-click-dropdown js-category-toggle">
-                    <div className="text-14 y-gap-15 js-dropdown-list">
-                      <div>
-                        <a href="#" className="d-block js-dropdown-link">
-                          Animation
-                        </a>
+                  <div
+                    className={
+                      isRatOpen
+                        ? "toggle-element -dropdown -dark-bg-dark-2 -dark-border-white-10 js-click-dropdown js-category-toggle -is-el-visible"
+                        : "toggle-element -dropdown -dark-bg-dark-2 -dark-border-white-10 js-click-dropdown js-category-toggle"
+                    }
+                  >
+                    <div class="text-14 y-gap-15 js-dropdown-list">
+                      <div onClick={() => setRatValue("great")}>
+                        <button class="d-block js-dropdown-link">Great</button>
                       </div>
 
-                      <div>
-                        <a href="#" className="d-block js-dropdown-link">
-                          Design
-                        </a>
+                      <div onClick={() => setRatValue("good")}>
+                        <button class="d-block js-dropdown-link">Good</button>
                       </div>
 
-                      <div>
-                        <a href="#" className="d-block js-dropdown-link">
-                          Illustration
-                        </a>
+                      <div onClick={() => setRatValue("medium")}>
+                        <button class="d-block js-dropdown-link">Medium</button>
                       </div>
 
-                      <div>
-                        <a href="#" className="d-block js-dropdown-link">
-                          Business
-                        </a>
+                      <div onClick={() => setRatValue("low")}>
+                        <button class="d-block js-dropdown-link">Low</button>
                       </div>
                     </div>
                   </div>
